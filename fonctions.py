5eb007431d65775fe1ad333edd233e8328c1abcb
Michael-bot521 <Michael-bot521@github.com>
2020-04-09 14:51:24 +0200

    MAJ instanciation laby/perso

--- fonctions.py
+++ fonctions.py
@@ -1,22 +1,119 @@
-class Macgyver:
-    def __init__(self, image, position):
-        self.image = image
-        self.start = start
-        self.position = image.get_rect()
-        
-    def moves(self):
-        # hero goes left with K_Left
-        # hero goes right with K_Right
-        # hero goes up with K_Up
-        # hero goes down with K_Down
-        # hero cannnot go on sprites with wall
-        
-    def take_item(self, item):
-        # hero take an item when arriving on item sprite
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-class Items:
-    def __init__(self, image, position):
-        self.image = image
-        self.position = position
+""" Classes and methods for the game Macgyver """
+
+import pygame
+from pygame.locals import *
+from constantes import *
+from labyrinthe import *
+from random import *
+import time
+
+side_sprite_number = 15
+sprite_size = 45
+window_size = side_sprite_number * sprite_size
+
+screen = pygame.display.set_mode((window_size, window_size)) 
+
+
+class Maze:
+    """class that build the maze"""
+    def __init__(self, level):
+        self.level = level
         
-    def 
+        level = [
+            [0, 0, 0, 1, 4, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+            [0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+            [0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, 0],
+            [0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0],
+            [1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+            [1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+            [1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0],
+            [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0],
+            [1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 1, 1],
+            [0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1],
+            [0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1],
+            [1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 7],
+            ]
+        
+    
+    def draw_maze(self, screen):
+        """drawing the maze"""
+
+        """Use surface "wall" to draw sprites with value == 1
+        and surface "floor" for sprites with value == 0""" 
+
+        for j, line in enumerate(level):
+            for i, sprite in enumerate(line):
+                if sprite == 1:
+                    screen.blit(wall, (i*45, j*45))
+                elif sprite == 0:
+                    screen.blit(floor, (i*45, j*45))
+                elif sprite == 4:
+                    screen.blit(start, (i*45, j*45))
+                elif sprite == 7:
+                    screen.blit(sentinelle, (i*45, j*45))
+                    
+"""   
+    def macgyver_tracker():
+        check if macgyver next move is not on wall
+    
+    def macgyver_end():
+        check if macgyver meet the guard
+        check if macgyver has found 3 items when showing to the guard"""
+      
+
+class MacGyver:
+    "class which creates main character"
+    def __init__(self, sprite):
+        # Character Sprites
+        self.sprite = pygame.image.load(sprite).convert_alpha()
+        # start position       
+        self.x = 188
+        self.y = 0
+        # Default direction side
+        
+        
+    def moves(self, direction):
+        
+
+        # Move to the right
+        if direction == "right":
+            # Check for not going out of bounds
+            # Move on next sprite
+
+            self.x += 45
+
+        elif direction == "left":
+                # Check for not going out of bounds
+                # Move on next sprite
+            self.x -= 45
+
+        elif direction == "down":
+                    # Check for not going out of bounds
+                    # Move on next sprite
+
+            self.y += 45
+
+        elif direction == "up":
+                        # Check for not going out of bounds
+                        # move on next sprite
+            self.y -= 45
+            
+        
+class Item:
+    def __init__(self, picture):
+        self.picture = pygame.image.load(picture).convert_alpha()
+        self.x = (randint(1, 15) * 45 + 2)
+        self.y = (randint(1, 15) * 45 + 2)
+        
+        
+        
+class Guard:
+    def __init__(self, sprite, position):
+        sprite
+        position
--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from fonctions import *
+from constantes import *
 from labyrinthe import *
 import time
 
@@ -9,76 +11,86 @@ from pygame.locals import *
 
 pygame.init()
 
-"""defaut settings"""
+""" game defaut settings """
 
-window_resolution = (676, 676)
-screen_surface = pygame.display.set_mode(window_resolution)
-SCREEN_WIDTH = 676
-SCREEN_HEIGHT = 676
-pygame.display.set_caption('McGyver Escape Game')
-macgyver = pygame.image.load("macgyver.png").convert_alpha()
-macgyver_block = macgyver.get_rect()
-macgyver_block.x = 323
-macgyver_block.y = 315
+# window settings
+side_sprite_number = 15
+sprite_size = 45
+window_size = side_sprite_number * sprite_size
 
-espace = pygame.image.load("espace.jpg").convert_alpha()
-espace = pygame.transform.scale(espace, (800, 600))
-font = pygame.font.Font(None, 36)
-text = font.render("Character moves Crash Test",30,(255,255,255))
-accueil_text = font.render("Press Space tab to go space,"
-                           " or C to continue", 50, (255, 255, 255))
-                           
-FOND      = (5, 10, 12)
+#window title
+window_title = "MacGyver escape game"
+screen = pygame.display.set_mode((window_size, window_size))
+
+# police settings for welcome and end screens
+text_size = 45
+text_col = [255,255,255]
+myfont = pygame.font.Font(None, text_size)
+text = myfont.render("Bravo, vous avez trouvé la sortie!", True, text_col)
+
+ether_sprite = pygame.image.load("ether.png").convert_alpha()
+ether_sprite = pygame.transform.scale(ether_sprite, (40, 40))
+tube_sprite = pygame.image.load("tube.png").convert_alpha()
+tube_sprite = pygame.transform.scale(tube_sprite, (40, 40))
+needle_sprite = pygame.image.load("needle.png").convert_alpha()
+needle_sprite = pygame.transform.scale(needle_sprite, (40, 40))
+syringue_sprite = pygame.image.load("syringue.png").convert_alpha()
+syringue_sprite = pygame.transform.scale(syringue_sprite, (40, 40))
+# character initialization
+macgyver = MacGyver("macgyver.png")
+
+# object initialization
+tube = Item("tube.png")
+needle = Item("needle.png")
+ether = Item("ether.png")
+
+# initialization of the maze
+maze = Maze(level)
+
+FOND = (10, 10, 10)
 
 # Main Loop
 
 run = 1
 while run:
+    
     for event in pygame.event.get():
         if not event.type == KEYDOWN:
-            screen_surface.blit(accueil_text, (75, 60))
-            pygame.display.flip()
+            maze.draw_maze(screen) 
+                                    
+            
         if event.type == QUIT:
             run = 0
         if event.type == KEYDOWN:
-            if event.key == K_SPACE:
-                print("Espace")
-                screen_surface.blit(espace, (0, 0))
-                pygame.display.flip()
-            if event.key == K_c:
-                draw_maze(screen_surface, level)
-                screen_surface.blit(text, (240, 440))
-                screen_surface.blit(macgyver, macgyver_block)
-                pygame.display.flip()                
+
+        
+            # checking for boundaries of the labyrinth so the 
+            # hero won't go out of bounds
+                
             if event.key == K_UP:
-                for y in range(45):
-                    time.sleep(.012)
-                    macgyver_block.y -= 1
-                    draw_maze(screen_surface, level)
-                    screen_surface.blit(macgyver, macgyver_block)
-                    pygame.display.flip()
-            if event.key == K_DOWN:
-                for y in range(45):
-                    time.sleep(.012)
-                    macgyver_block.y += 1
-                    draw_maze(screen_surface, level)
-                    screen_surface.blit(macgyver, macgyver_block)
-                    pygame.display.flip()
-            if event.key == K_RIGHT:
-                for x in range(45):
-                    time.sleep(.012)
-                    macgyver_block.x += 1
-                    draw_maze(screen_surface, level)
-                    screen_surface.blit(macgyver, macgyver_block)
-                    pygame.display.flip()    
-            if event.key == K_LEFT:
-                for x in range(45):
-                    time.sleep(.012)
-                    macgyver_block.x -= 1
-                    draw_maze(screen_surface, level)
-                    screen_surface.blit(macgyver, macgyver_block)
-                    pygame.display.flip()
-             
-    pygame.display.flip()       
+                macgyver.moves("up")
+                    
+            elif event.key == K_DOWN:
+                macgyver.moves("down")
+                    
+            elif event.key == K_RIGHT:
+                macgyver.moves("right")                  
+                                        
+            elif event.key == K_LEFT:
+                macgyver.moves("left")
+            
+        screen.fill(FOND)                      
+        maze.draw_maze(screen)                                  # setting maze
+        screen.blit(macgyver.sprite, (macgyver.x, macgyver.y))  # refreshing of macgyver position
+        screen.blit(tube_sprite, (tube.x, tube.y))
+        screen.blit(needle_sprite, (needle.x, needle.y))
+        screen.blit(ether_sprite, (ether.x, ether.y))
+        pygame.display.flip()
+        
+    if macgyver.x > 600 and macgyver.y > 600:
+            
+        screen.fill(FOND)
+        screen.blit(text, (90, 280))
+        pygame.display.flip()
 
 pygame.quit()
